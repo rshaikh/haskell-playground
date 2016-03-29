cc6fbfec80144c83f3a75044a86f7c6d1c575896
rashid <rshaikh@equalexperts.com>
2016-03-29 12:43:19 +0530

    Initial commit

---
+++ scratchpad.hs
@@ -0,0 +1,56 @@
+doubleMe :: Int -> Int
+doubleMe x = x + x
+
+doubleSmallNumber x = if x > 100
+                      then x
+                      else x * 2
+
+rashid'sFunction = 10
+
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase xs = [x | x <- xs, x `elem` ['A'..'Z']]
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN"
+lucky x = "Sorry, you are out of luck"
+
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One"
+sayMe 2 = "Two"
+sayMe 3 = "Three"
+sayMe x = "Not between 1 - 3"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial x = x * factorial(x - 1)
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+        | bmi <= 18.5 = "Underweight"
+        | bmi <= 25.0 = "Normal"
+        | bmi <= 30.0 = "Fat"
+        | otherwise = "Whale"
+        where bmi = weight / height ^ 2
+
+
+
+initials :: String -> String -> String
+initials firstname lastname  = [f] ++ " ." ++ [l] ++ "."
+          where (f:_) = firstname
+                (l:_) = lastname
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "empty list"
+maximum' [x] = x
+maximum' (x:xs)
+          | x > maxFromTail = x
+          | otherwise = maxFromTail
+          where maxFromTail = maximum' xs
+
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+          | n <= 0    = []
+          | otherwise = x : replicate' (n - 1) x
