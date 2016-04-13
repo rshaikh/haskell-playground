2dafebc2adbffa9d5e380c923c5802c9ad637b67
rashid <rshaikh@equalexperts.com>
2016-04-13 14:33:14 +0530

    add reverse function using fold

--- scratchpad.hs
+++ scratchpad.hs
@@ -61,3 +61,7 @@ compareWithHundred x = compare 100 x
 
 compareWithHundred' :: (Num a, Ord a) => a -> Ordering
 compareWithHundred' = compare 100
+
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
